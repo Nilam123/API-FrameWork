8131a0d6c8a9d617bdcd77fa47c086c3ba809854
91840 <91840@LAPTOP-M3HSMS1J>
2023-03-30 13:21:36 +0530

    //some changes

--- src/test/java/Practice/PracticeTest.java
+++ src/test/java/Practice/PracticeTest.java
@@ -8,6 +8,10 @@ public class PracticeTest {
 public void PracticeTestScript()
 {
 	
-System.out.println("JENKINS CONFUGURATION DONW");
+System.out.println("JENKINS CONFUGURATION DONE");
+
+System.out.println(">>>>>>>>>PUSH YOUR MAIN CODE ");
+
+System.out.println(">>>>>>>>>YOU CAN DO IT  ");
 }
 }
